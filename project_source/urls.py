443510fcdd25ec204728a05656c6ba952f4f7fe6
siyam04 <galib.abdullah04@gmail.com>
2019-03-10 21:29:36 +0600

    first commit

---
+++ project_source/urls.py
@@ -0,0 +1,42 @@
+"""project_source URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf import settings
+from django.urls import path, include
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+
+    # Admin dashboard path
+    path('admin/', admin.site.urls),
+
+    # # user_authentication app's path
+    # path('user/', include('user_authentication.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+# Project source URL Debugging integration
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+
+        path('__debug__/', include(debug_toolbar.urls)),
+
+    ] + urlpatterns
+
